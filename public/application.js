e210348f8ea5e3a98f302c5ff2e76ddcd0f441a2
Luciano Krebs <luciano.krebs@hotmail.com>
2017-07-11 08:13:11 -0300

    adding small style to the web application

--- public/application.js
+++ public/application.js
@@ -5,4 +5,8 @@ $(document).ready(function() {
   $('#trig').on('click', function() {
     $(".last").fadeIn(1500);
   });
+
+  $("#food").on("click", function() {
+    alert("Food Imported :)");
+  });
 });
